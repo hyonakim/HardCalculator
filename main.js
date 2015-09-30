67cff9a2f01a98e4274c6c2e2f245ed62c089eac
Hyona Kim <tekkieninja@gmail.com>
2015-09-30 00:00:41 -0400

    Modified HTML and CSS

---
+++ main.js
@@ -0,0 +1,50 @@
+//Grabbing the elements
+var num0 = document.querySelector('.number0');
+var num1 = document.querySelector('.number1');
+var num2 = document.querySelector('.number2');
+var num3 = document.querySelector('.number3');
+var num4 = document.querySelector('.number4');
+var num5 = document.querySelector('.number5');
+var num6 = document.querySelector('.number6');
+var num7 = document.querySelector('.number7');
+var num8 = document.querySelector('.number8');
+var num9 = document.querySelector('.number9');
+var numDash = document.querySelector('.numberdash');
+var numSlash = document.querySelector('.numberslash');
+var numPeriod = document.querySelector('.numberperiod');
+var numEquals = document.querySelector('.numberequals');
+var numPlus = document.querySelector('.numberplus');
+var numX = document.querySelector('.numberX');
+var express = document.querySelector('#expression')
+
+
+
+//Perform Calculation 
+var addition = function () {
+  
+}
+
+//Show the Number
+showNumber = function () {
+  
+}
+
+
+//Button Click 
+num0.addEventListener('click', calculation);
+num1.addEventListener('click', calculation);
+num2.addEventListener('click', calculation);
+num3.addEventListener('click', calculation);
+num4.addEventListener('click', calculation);
+num5.addEventListener('click', calculation);
+num6.addEventListener('click', calculation);
+num7.addEventListener('click', calculation);
+num8.addEventListener('click', calculation);
+num9.addEventListener('click', calculation);
+numDash.addEventListener('click', calculation);
+numSlash.addEventListener('click', calculation);
+numPeriod.addEventListener('click', calculation);
+numEquals.addEventListener('click', calculation);
+numPlus.addEventListener('click', calculation);
+numX.addEventListener('click', calculation);
+express.addEventListener('click', calculation);
